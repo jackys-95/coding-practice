f099730232950977f79102729b5271059a8eccc6
Jacky Shum <j_shum55@outlook.com>
2017-08-04 13:37:53 -0400

    Fixed two_sorted_array_median to work for n=m sized arrays

--- algorithms/divide and conquer/two_sorted_array_median.py
+++ algorithms/divide and conquer/two_sorted_array_median.py
@@ -14,7 +14,10 @@ def two_sorted_arrays_median(a, b):
         median = median_a
     else:
         if (len(a) == 1 and len(b) == 1):
-            median = get_median(a.append(b[0]))
+            a.append(b[0])
+            median = get_median(a)
+        elif (len(a) == 2 and len(b) == 2):
+            median = median_of_four_elements(a, b)
         elif (median_a > median_b):
             median = two_sorted_arrays_median(a[0:calculate_slice_index(length_a)], \
                                               b[calculate_slice_index(length_b):])
@@ -23,6 +26,13 @@ def two_sorted_arrays_median(a, b):
                                               a[calculate_slice_index(length_a):])
     return median
 
+def median_of_four_elements(a, b):
+    '''
+    Finds the two middle elements and calculates median
+    '''
+    max_num = (max(a[0], max(a[1], max(b[0], b[1]))))
+    min_num = (min(a[0], min(a[1], min(b[0], b[1]))))
+    return (a[0] + a[1] + b[0] + b[1] - max_num - min_num) / 2.0
 def calculate_slice_index(length):
     if (length == 0):
         return None
@@ -37,11 +47,10 @@ def get_median(x):
     '''
     Gets the median of a sorted list x
     '''
-
     length = len(x)
     if (length == 0):
         return None
     elif (length % 2 == 0):
-        return (x[length // 2] + x[(length//2) - 1])/2
+        return (x[length // 2] + x[(length // 2) - 1])/2
     else:
         return x[length // 2]
