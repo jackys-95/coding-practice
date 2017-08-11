3b934e9335d2bb176dfb0d7bfaf180026b52d685
Jacky Shum <j_shum55@outlook.com>
2017-08-11 18:27:41 -0400

    Added trie stub

---
+++ algorithms/Graphs and Trees/trie.py
@@ -0,0 +1,16 @@
+from graph import Vertex
+
+class Trie:
+    def __init__(self):
+        self.root = Vertex('')
+        self.vertices = { '': self.root }
+
+    def add(self, item):
+        '''
+        '''
+
+    def is_member(self, item):
+        '''
+        '''
+
+    def remove(self, item):
